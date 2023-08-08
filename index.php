c7ccdbd968da43c71610ec2cabe0c57e8b3c6280
okrafaldino <okrafaldino@gmail.com>
2023-08-08 15:02:59 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, maiores earum! Dignissimos aliquam dolore unde eaque nam aperiam itaque necessitatibus, ducimus ab expedita ullam repellat odit reprehenderit. Eaque, aut voluptatum.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 
 </html>
